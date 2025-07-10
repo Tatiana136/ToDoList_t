405215fd4b608f6418b8d635671f4c2710f76515
Tatiana <154363892+Tatiana136@users.noreply.github.com>
2025-07-10 14:38:41 +0300

    Update permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -16,12 +16,6 @@ class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        # getattr — функция возвращает значение атрибута объекта (admin - имя атрибута, который хотим получить)
-        # Если у пользователя есть атрибут admin (например, булево поле в модели User или is_admin), 
-        # то getattr вернёт его значение (True или False).
-        # Если атрибута нет (например, в стандартной модели User его нет), то getattr вернёт False,
-        # чтобы не получить ошибку AttributeError.
         if request.user.is_authenticated and getattr(request.user, 'admin', False):
             return True
-        # Разрешить изменение и удаление только автору
         return obj.author == request.user
